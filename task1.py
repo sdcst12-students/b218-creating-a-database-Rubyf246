b28084dcdffa4253bc735921c3bac490d4e6c27b
Rubyf246 <144410608+Rubyf246@users.noreply.github.com>
2025-04-05 19:29:36 -0700

    done

--- task1.py
+++ task1.py
@@ -113,53 +113,101 @@ result = cursor.fetchall()
 print ("----------------Initial Visits:------------------");
 for i in result:
     print(i)
-
-
-
     
-    
-#################  Add a new customer ###################
-print ("\n----------------Add a customer :------------------");    
-
 
 def insert_customer (fname, lname, ph, email, addr, city, pcode):
     cur = connection.cursor()
     cur.execute("insert into customers (fname, lname, phone, email,  address, city, postalcode) values (?,?,?,?,?,?,?);", (fname, lname, ph, email, addr, city, pcode))
     connection.commit()
 
+    
 
-query = "select * from customers"
-cursor.execute(query)
-result = cursor.fetchall()
 
-print ("\n----------------After Adding New Customer:------------------")
 
-for i in result:
-    print(i)
-
-#################  Search by email ###################   
-
-def search_customer_by_email (email): 
-    cursor = connection.cursor()
-    query = """select * from customers where email = ?"""
-    cursor.execute(query, (email,))
-    records = cursor.fetchall()
-    print("Search by email:  ", email)
-    for row in records:
-        print("First Name = ", row[1])
-        print("Last Name  = ", row[2])
-    cursor.close()
 
 
     
+    
+     
+#################  Search by email ###################   
+
+##def search_customer_by_email (email): 
+##    cursor = connection.cursor()
+##    query = """select * from customers where email = ?"""
+##    cursor.execute(query, (email,))
+##    records = cursor.fetchall()
+##    print("Search by email:  ", email)
+##    for row in records:
+##        print("First Name = ", row[1])
+##        print("Last Name  = ", row[2])
+##    cursor.close()
+    
+def search_customer (search_field, search_by_value):
+    ## decide what query is, based on search field
+    
+    if search_field == "email":
+        query = """select * from customers where email = ?"""
+    elif search_field == "first name":
+        query = """select * from customers where fname = ?"""
+    elif search_field == "last name":
+        query = """select * from customers where lname = ?"""
+    elif search_field == "phone number":
+        query = """select * from customers where phone = ?"""    
+    elif search_field == "email":
+        query = """select * from customers where email = ?"""        
+    elif search_field == "address":
+        query = """select * from customers where address = ?"""        
+    elif search_field == "city":
+        query = """select * from customers where city = ?"""        
+    elif search_field == "postal code":
+        query = """select * from customers where postalcode = ?"""  
+    elif search_field == "id":
+        query = """select * from customers where id = ?"""           
+
+    cursor = connection.cursor()
+    cursor.execute(query, (search_by_value,))
+    records = cursor.fetchall()
+    print(f"Search by {search_field}:  ", search_by_value)
+    for row in records:
+        print("ID = ", row[0])
+        print("First Name = ", row[1])
+        print("Last Name  = ", row[2])
+        print("Phone = ", row[3])
+        print("Email  = ", row[4])
+        print("Address = ", row[5])
+        print("City  = ", row[6])
+        print("Postal Code  = ", row[7])
+    cursor.close()
+
 ##################  Ask which function they want to excute  ################
 
 choice = input("Input your choice (search/add) ---->    ")
 if choice == "search":
-    search_customer_by_email (inpt_email = input("Enter email: "))
+    field = input ("What do you want to search by:    ") # ask the user what category they want to search by and out it into field 
+    if field not in ("id", "first name", "last name", "phone number", "email", "city", "address", "postal code"):
+        print ("Invalid entry search. Try again")
+    else:
+        # ask the user what category they want to search by and out it into value
+        value = input ("what is your value you want to search by:    ") 
+        search_customer (field,value)
+        
 else: 
-    insert_customer(inpt_fname = input("Enter first name: "), inpt_lname = input("Enter last name: "), inpt_ph = input("Enter phone: "), inpt_email = input("Enter email: "), inpt_addr = input("Enter address: "), inpt_city = input("Enter city: "), inpt_pcode = input("Enter postal code: "))
-
-
+   #################  Add a new customer ###################
+    print ("\n----------------Add a customer :------------------");    
+    inpt_fname = input("Enter first name: ")
+    inpt_lname = input("Enter last name: ")
+    inpt_ph = input("Enter phone: ")
+    inpt_email = input("Enter email: ")
+    inpt_addr = input("Enter address: ")
+    inpt_city = input("Enter city: ")
+    inpt_pcode = input("Enter postal code: ")
+    insert_customer(inpt_fname,inpt_lname, inpt_ph ,inpt_email, inpt_addr,inpt_city,inpt_pcode)
+    print ("\n----------------After Adding New Customer:------------------")
+    #displays info after inserts
+    query = "select * from customers"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for i in result:
+        print(i)
 
 
